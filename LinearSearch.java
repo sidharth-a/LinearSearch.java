32199b2827c38d74c608acf83edeea815e4c1fdc
sidharth-a <56468787+sidharth-a@users.noreply.github.com>
2020-10-01 19:55:29 +0530

    Modified the program to take array elements and the element to be searched as input from the user.
    Also changed the program so it prints if the element to be searched is present multiple times

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,24 +1,31 @@
+import java.util.Scanner;
 public class LinearSearch {
-    public static int search(int arr[], int x)
+    public static void search(int arr[], int x, int n)
     {
-        int n = arr.length;
+        int count = 0;
         for(int i=0; i<n; i++)
         {
-            if(arr[i] == x)
-                return i;
+            if(arr[i] == x){
+                count++;
+                System.out.println("Element is present at index " +i);
+            }
         }
-        return -1;
+        if(count==0)
+            System.out.print("Element is not present in the array.");
     }
 
     public static void main(String args[])
     {
-        int arr[] = {2, 3, 4, 10, 40};
-        int x = 10;
-
-        int result = search(arr, x);
-        if (result == -1)
-            System.out.print("Element is not present in the array.");
-        else
-            System.out.print("Element is present at index " + result);
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number of terms : ");
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Enter the array elements : ");
+        for(int i=0;i<n;i++)
+            arr[i]=scan.nextInt();
+        System.out.print("Enter the element to be searched : ");
+        int x = scan.nextInt();
+        search(arr, x, n);
     }
 }
